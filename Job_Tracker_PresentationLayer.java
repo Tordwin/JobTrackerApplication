8a5880c5a03c4b5dbcf7f03715ddfefc905a047e
tordwin <ec7233@g.rit.edu>
2025-12-12 13:50:47 -0500

    starting GUI and connecting it to datalayer

--- Job_Tracker_PresentationLayer.java
+++ Job_Tracker_PresentationLayer.java
@@ -1,10 +1,14 @@
 import java.awt.*;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class Job_Tracker_PresentationLayer {
     Job_Tracker_DataLayer dataLayer = new Job_Tracker_DataLayer();
@@ -14,35 +18,52 @@ public class Job_Tracker_PresentationLayer {
         JPanel loginBox = new JPanel(new GridLayout(3, 2));
         JLabel userInput = new JLabel("Username: ");
         JLabel passInput = new JLabel("Password: ");
-        JTextField user = new JTextField("root");
-
-        JTextField pass = new JPasswordField("");
         JLabel databaseInput = new JLabel("Database: ");
+
+        JTextField user = new JTextField("root");
+        JTextField pass = new JPasswordField("");
         JTextField database = new JTextField("job_application_db");
 
-        loginBox.add(userInput);
-        loginBox.add(user);
-        loginBox.add(passInput);
-        loginBox.add(pass);
-        loginBox.add(databaseInput);
-        loginBox.add(database);
+        loginBox.add(userInput); loginBox.add(user);
+        loginBox.add(passInput); loginBox.add(pass);
+        loginBox.add(databaseInput); loginBox.add(database);
 
-        JOptionPane.showMessageDialog(null, loginBox,
-            "Job Application Tracker", JOptionPane.QUESTION_MESSAGE);
+        JOptionPane.showMessageDialog(null, loginBox, "Job Application Tracker", JOptionPane.QUESTION_MESSAGE);
 
         String username = user.getText();
         String databaseName = database.getText();
-
-        String passwordInput = new String();
-
-        passwordInput = pass.getText();
+        String passwordInput = new String(pass.getText());
 
         // Set the default password to "student" even if the user doesn't enter anything
         if (passwordInput.equalsIgnoreCase("")) {
         } else {
             passwordInput = pass.getText();
         }
-        dataLayer.connect(databaseName, username, passwordInput);
+        Boolean connection = dataLayer.connect(databaseName, username, passwordInput);
+        if (!connection) {
+            JOptionPane.showMessageDialog(null, "Database connection failed.", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        showMainMenu();
+    }
+
+
+    private void showMainMenu() {
+        JFrame frame = new JFrame("Job Application Tracker - Main Menu");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+
+        JLabel title = new JLabel("Please select an option from the menu:", SwingConstants.CENTER); 
+        title.setFont(defaultFont);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(0, 1, 10, 10));
+
+        JButton getButton = new JButton("Get Applications");
+        JButton addButton = new JButton("Add Applications");
+        JButton deleteButton = new JButton("Delete Applications");
+        JButton updateButton = new JButton("Update Applications");
     }
 
     public static void main(String[] args) {
